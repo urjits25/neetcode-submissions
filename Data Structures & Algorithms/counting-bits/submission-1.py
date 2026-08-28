917207e5af14251133548d8aa79c3d1fba2eb6e4
Urjit Sardesai <urjit.s25@gmail.com>
2026-08-27 20:36:07 -0700

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0, 1, 1, 2]
+        if n < 4:
+            return dp[ : n+1 ]
+        
+        cp2, ncp2 = 4, 8
+        for cur in range(4, n+1):
+            if cur < ncp2:
+                dp.append(1 + dp[cur-cp2])
+            else:
+                dp.append(1)
+                cp2, ncp2 = ncp2, ncp2 * 2
+
+        return dp
+
